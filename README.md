6e3e392b4c6e61a9edd1812ca12619fe12ed13c6
Amanda Cristina <amandacris_lima@outlook.com>
2022-01-25 19:38:23 -0300

    Criando um app para apresentar seu potfólio do gitHub

---
+++ README.md
@@ -0,0 +1,2 @@
+# LAB - Criando um App para Apresentar seu Portfólio do GitHub
+O curso pode ser acessado na plataforma da [Digital Innovation One](https://digitalinnovation.one/).
---
+++ app/src/main/java/br/com/dio/app/repositories/ui/MainActivity.kt
@@ -0,0 +1,69 @@
+package br.com.dio.app.repositories.ui
+
+import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
+import br.com.dio.app.repositories.R
+import br.com.dio.app.repositories.core.createDialog
+import br.com.dio.app.repositories.core.createProgressDialog
+import br.com.dio.app.repositories.core.hideSoftKeyboard
+import br.com.dio.app.repositories.databinding.ActivityMainBinding
+import br.com.dio.app.repositories.presentation.MainViewModel
+import org.koin.androidx.viewmodel.ext.android.viewModel
+
+class MainActivity : AppCompatActivity(), SearchView.OnQueryTextListener {
+
+    private val dialog by lazy { createProgressDialog() }
+    private val viewModel by viewModel<MainViewModel>()
+    private val adapter by lazy { RepoListAdapter() }
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+        binding.rvRepos.adapter = adapter
+
+        viewModel.repos.observe(this) {
+            when (it) {
+                MainViewModel.State.Loading -> dialog.show()
+                is MainViewModel.State.Error -> {
+                    createDialog {
+                        setMessage(it.error.message)
+                    }.show()
+                    dialog.dismiss()
+                }
+                is MainViewModel.State.Success -> {
+                    dialog.dismiss()
+                    adapter.submitList(it.list)
+                }
+            }
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        val searchView = menu.findItem(R.id.action_search).actionView as SearchView
+        searchView.setOnQueryTextListener(this)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onQueryTextSubmit(query: String?): Boolean {
+        query?.let { viewModel.getRepoList(it) }
+        binding.root.hideSoftKeyboard()
+        return true
+    }
+
+    override fun onQueryTextChange(newText: String?): Boolean {
+        Log.e(TAG, "onQueryTextChange: $newText")
+        return false
+    }
+
+    companion object {
+        private const val TAG = "TAG"
+    }
+
+}
